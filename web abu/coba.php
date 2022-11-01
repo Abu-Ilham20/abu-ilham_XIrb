d710a1a05a280c8306be9c21de4d0d6c3d3fa5b5
Abu-Ilham20 <115514100+Abu-Ilham20@users.noreply.github.com>
2022-11-01 10:45:43 +0700

    Add files via upload

---
+++ web abu/coba.php
@@ -0,0 +1,23 @@
+<?php 
+// inisiasi dan inisialisasi variabel
+$a = 10;
+$b = 5;
+$c = $a + 5;
+$d = $b + (10 * 5);
+$e = $d - $c;
+
+//Tampilkan data dengan perintah echo
+echo "variabel a: ($a) <br>";
+echo "variabel b: ($b) <br>";
+echo "variabel c: ($c) <br>";
+echo "variabel d: ($d) <br>";
+echo "variabel e: ($e) <br>";
+
+//mengetahui tipe data dengan variabel
+var_dump($e);
+
+$nilai = 8.7;
+$nilai2 = 9.2;
+
+echo "nilai = {$nilai} <br>";
+?>
